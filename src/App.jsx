ceac98806dbec636be23156d9f8cb145d52c4ee4
Swapvp <swapneshpawar1994@gmail.com>
2024-04-23 12:16:28 +0530

    color generator

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import RandomColor from "./Component/RandomColor";
+
+function App() {
+  return (
+    <>
+      <RandomColor></RandomColor>
+    </>
+  );
+}
+
+export default App;
